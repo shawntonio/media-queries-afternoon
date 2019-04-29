2e339ced20b4b4ac88fa942301bda2a3f2a367e2
Shawn Turley <antonioturley@gmail.com>
2019-04-29 16:59:27 -0600

    menu toggle

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const headers = document.getElementsByTagName('header')
 const header = headers[0]
 
 const sections = document.getElementsByTagName('section')
-const lis = document.getElementsByTagName('li')
-
+const lis = document.getElementsByClassName('list')
+const lis2 = document.getElementsByClassName('list2')
 
 const h1 = document.getElementById('logo')
+const menu = document.getElementById('menu')
+const links = document.getElementById('links')
 
 function toggleHeader() {
 	// console.dir(sections[1])
@@ -27,9 +29,23 @@ function scrollToSection(section) {
 	})
 }
 
+function toggleMenu() {
+	if (links.classList.contains('show-list')) {
+		links.classList.remove('show-list')
+		links.classList.add('hide-list')
+	} else {
+		links.classList.remove('hide-list')
+		links.classList.add('show-list')
+	}
+}
+
 window.addEventListener('scroll', toggleHeader)
+menu.addEventListener('click', toggleMenu)
 
 for (let i = 0; i < lis.length; i++) {
 	lis[i].addEventListener('click', () => scrollToSection(i))
+}
+for (let i = 0; i < lis2.length; i++) {
+	lis2[i].addEventListener('click', () => scrollToSection(i))
 }
 
